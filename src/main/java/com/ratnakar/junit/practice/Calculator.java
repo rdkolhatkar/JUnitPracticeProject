4db32d840698cc971ccfafce171159fc72f96886
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-22 17:41:42 +0530

    first commit

---
+++ src/main/java/com/ratnakar/junit/practice/Calculator.java
@@ -0,0 +1,10 @@
+package com.ratnakar.junit.practice;
+
+// This class is called class under test
+public class Calculator {
+
+    // This method is called method under test
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/com/ratnakar/junit/test/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.ratnakar.junit.test;
+
+import com.ratnakar.junit.practice.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    public void addTest() {
+        int a = 20;
+        int b = 30;
+        Calculator calculator = new Calculator();
+        int result = calculator.add(a, b);
+        assertEquals(result, 50);
+    }
+}
